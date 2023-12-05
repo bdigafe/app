e882ff983d7b86033d85d829c6b321fcff7716a0
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-05 03:37:45 -0500

    Init

---
+++ pages/bygenre.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+
+# Movies column_config
+movies_column_config = {
+    "image_url" : st.column_config.ImageColumn(label="Title",  width=None, help=None),
+    "Title" : st.column_config.TextColumn(label="Title",  width=None, help=None),
+}
+
+st.set_page_config(
+    page_title="Movie Recommender"
+)
+
+@st.cache_data
+def load_movies():
+    # Movies: MovieID::Title::Genres
+    movies = pd.read_csv(f'./data/movies.dat', sep='::', engine = 'python', encoding="ISO-8859-1", header = None)
+    movies.columns = ['MovieID', 'Title', 'Genres']
+
+    # Top 10 Movies for each genre
+    df = pd.read_csv('./data/top10_movies.csv')
+    df = df.merge(movies, on='MovieID', how='inner', suffixes=("", "_y"),)
+    
+    df['image_url'] = df.apply(lambda m: f'<a href="./images/{m.MovieID}.jpg">{m.Title}</a>', axis=1)
+    return df
+
+@st.cache_data
+def get_top_movies_by_genre(df, genre):
+    return df[df['Genres'] == genre]
+
+# Load Top Movies
+movies = load_movies()
+
+# Title
+st.header('Movie Recommender')
+
+# Selection
+st.subheader('Step 1: Select a Genre')
+genre = st.selectbox('', movies['Genres'].unique())
+
+# Button to trigger the recommendation
+st.subheader('Step 2: Click to get your recommendations')
+
+# Render recommendation
+if st.button('Get Recommendations'):
+    top_movies = get_top_movies_by_genre(movies, genre)
+    st.data_editor(top_movies, column_order=['image_url', 'Title'], column_config= movies_column_config, hide_index=True)
+    
---
+++ pages/recommender.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(
+    page_title="Recommendation by Rating"
+)
+
+@st.cache_data
+def load_top_sim(movies):
+    sim = pd.read_csv('./data/top_sim.csv')
+    return sim
+
+@st.cache_data
+def load_movies():
+    # Movies: MovieID::Title::Genres
+    movies = pd.read_csv(f'./data/movies.dat', sep='::', engine = 'python', encoding="ISO-8859-1", header = None)
+    movies.columns = ['MovieID', 'Title', 'Genres']
+    return movies
+
+@st.cache_data
+def get_movie_samples(sim, movies, sample_size=200):
+    df = sim.copy()
+    df.fillna(0, inplace=True)
+    df['avg_rating'] = df.mean(axis=1)
+
+    # Get top movies by their average rating
+    df = df.sort_values(by='avg_rating', ascending=False).head(sample_size)
+    
+    # Merge with movies to get the movie titles
+    df = df.merge(movies, on='MovieID')
+    return df[['MovieID', 'Title']]
+
+@st.cache_data
+def render_movie_samples(sample_movies):
+    st.markdown('## Top 10 Movies by Rating')
+    st.table(sample_movies)
+
+def myIBCF(S, w, t=None):
+
+    # Create a mask with for values w where w is a nan
+    wmask = w.copy()
+    wmask[wmask.isna()] = 0
+    wmask[wmask != 0] = 1
+
+    # Fill all nan values with 0
+    wu = w.fillna(0)
+    su = S.fillna(0)
+
+    # Compute the weighted average of wi*si
+    R = su.multiply(wu, axis=1).sum(axis=1) 
+    D = su.multiply(wmask, axis=1).sum(axis=1)
+    r = R/D
+
+    # Remove movies that have already been rated
+    r.loc[wu > 0] = np.nan
+
+    # return the movie ids with the top 10 ratings
+    return r.sort_values(ascending=False)
+
+# Load Movies
+movies = load_movies()
+st.write(movies)
+
+# Load the data
+sim = load_top_sim(movies)
+st.write(sim.head())
+
+# Get the movie samples
+samples = get_movie_samples(sim, movies)
+
+# Render the samples
