3e1f0e51a7542575fae5893aa971cb7f63509b0b
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-07 11:15:23 -0500

    uu

--- pages/recommender.py
+++ pages/recommender.py
@@ -62,7 +62,7 @@ def save_rating(key):
     if not key in st.session_state:
         return
     
-    if st.session_state[key] == 0 and key in st.session_state.ratings:
+    if st.session_state[key] == "0" and key in st.session_state.ratings:
         del st.session_state.ratings[key]
     else:
         st.session_state.ratings[key] = st.session_state[key]
