6da144b8d39d338b68a7b0b7276cf95fb7eb8038
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-06 23:50:25 -0500

    ii

--- pages/recommender.py
+++ pages/recommender.py
@@ -1,7 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from limited_dict import LimitedDict
+from collections import OrderedDict
+
+# From: https://gist.github.com/davesteele/44793cd0348f59f8fadd49d7799bd306
+class LimitedDict(OrderedDict):
+    """Dict with a limited length, ejecting LRUs as needed."""
+
+    def __init__(self, *args, cache_len: int = 10, **kwargs):
+        assert cache_len > 0
+        self.cache_len = cache_len
+
+        super().__init__(*args, **kwargs)
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        super().move_to_end(key)
+
+        while len(self) > self.cache_len:
+            oldkey = next(iter(self))
+            super().__delitem__(oldkey)
+
+    def __getitem__(self, key):
+        val = super().__getitem__(key)
+        super().move_to_end(key)
+
+        return val
 
 st.set_page_config(
     layout="wide",
