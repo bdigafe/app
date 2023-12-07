30c3dfefb4567b572189384d48533f29487eff68
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-07 02:26:42 -0500

    u

--- pages/recommender.py
+++ pages/recommender.py
@@ -88,7 +88,7 @@ def render_movie_samples(sample_movies):
 
         # Add Slider
         col.slider(
-            label=None
+            label=None,
             min_value=value,
             max_value=5,
             value=0,
