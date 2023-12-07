6104c7357092c92b6a3cec6475541adbd7d52ba8
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-07 02:52:00 -0500

    uu

--- pages/recommender.py
+++ pages/recommender.py
@@ -86,7 +86,7 @@ def render_movie_samples(sample_movies):
             value = st.session_state.ratings[MovieID]
 
         # Add Slider
-        col.st_star_rating("Please rate you experience", maxValue=5, defaultValue=3, key="_"+MovieID)
+        col.st_star_rating("Please rate you experience", maxValue=5, defaultValue=3, key=f"_{MovieID}")
 
         col.slider(
             label='Rating',
