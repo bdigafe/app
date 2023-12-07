1d0f66ec2541b1b1be17f66fd0211be891a3f08f
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-07 02:03:46 -0500

    ii

--- pages/recommender.py
+++ pages/recommender.py
@@ -65,14 +65,11 @@ def save_rating(key):
 def render_movie_samples(sample_movies):
     st.markdown('## Top 10 Movies by Rating')   
     i=1
-    #div = st.container() 
-    grid = st.grid()
-    div = grid.row()
+    div = st.container() 
     cols = div.columns([2, 2, 2])                       
     for _, row in sample_movies.iterrows():
         if (i) % 3 == 0:
-            #div = st.container() 
-            div = grid.row()
+            div = st.container() 
             cols = div.columns([2, 2, 2]) 
 
         col = cols[i % 3].container(border=True)
