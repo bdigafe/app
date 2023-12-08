c9907b06cb224df5f0eb5cf02e9c1c2964bf3030
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-08 15:43:20 -0500

    qq

--- pages/bygenres.py
+++ pages/bygenres.py
@@ -60,7 +60,7 @@ genre = st.selectbox('Select Genres from the list', movies['Genres'].unique())
 # Render recommendation
 if st.button('Get Recommendations'):
     top_movies = get_top_movies_by_genre(movies, genre)
-    div = st.container(border=True)
+    div = st.container(border=False)
     render_movies(top_movies, div)
    
      
