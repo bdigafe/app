bf13026096021a70503291b692247a338f801365
bdigafe@gmail.com <bdigafe@gmail.com>
2023-12-07 19:41:06 -0500

    uu

--- pages/recommender.py
+++ pages/recommender.py
@@ -186,7 +186,7 @@ st_top_movies = st.expander("Recommendations", expanded=True)
 
 # Indicate number of ratings
 st.sidebar.slider(
-    label=':red[You have rated]',
+    label=':blue[You have rated]',
     min_value=1,
     max_value=10,
     value=len(st.session_state.ratings),
